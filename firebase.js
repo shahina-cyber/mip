e1250b94baf85a5f81d9b58df54af9da72fec2f1
shahina-cyber <shahinaibrahim0812@gmail.com>
2026-03-06 14:23:41 +0530

    firebase added

--- firebase.js
+++ firebase.js
@@ -1,23 +1,56 @@
-// Import Firebase
+// Import Firebase SDKs
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-import { getDatabase } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-database.js";
+import { 
+    getFirestore, 
+    collection, 
+    addDoc, 
+    getDoc, 
+    getDocs, 
+    doc, 
+    setDoc, 
+    updateDoc, 
+    onSnapshot, 
+    query 
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+import { 
+    getAuth, 
+    signInWithEmailAndPassword, 
+    createUserWithEmailAndPassword, 
+    onAuthStateChanged, 
+    signOut 
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 
-// Firebase config
 const firebaseConfig = {
-  apiKey: "AIzaSyDyptaMe1PQZgDNJ2OzUILoAhZTJLbpTro",
-  authDomain: "complaint-system-2-8c113.firebaseapp.com",
-  databaseURL: "https://complaint-system-2-8c113-default-rtdb.firebaseio.com",
-  projectId: "complaint-system-2-8c113",
-  storageBucket: "complaint-system-2-8c113.firebasestorage.app",
-  messagingSenderId: "220960024495",
-  appId: "1:220960024495:web:6c6bafc62a0398d3bf715a"
-};
+    apiKey: "AIzaSyAAG2bHKBAYBwoHr1m5RKMpI9HARx0TcWA",
+    authDomain: "cms01-d8400.firebaseapp.com",
+    databaseURL: "https://cms01-d8400-default-rtdb.firebaseio.com",
+    projectId: "cms01-d8400",
+    storageBucket: "cms01-d8400.firebasestorage.app",
+    messagingSenderId: "761545517220",
+    appId: "1:761545517220:web:3a16347bc0bfd2a3e35e31",
+    measurementId: "G-W6KWVF3DBD"
+  };
 
-// Initialize Firebase
+// Initialize Firebase services
 const app = initializeApp(firebaseConfig);
+const db = getFirestore(app); // Firestore instance
+const auth = getAuth(app);
 
-// Connect Realtime Database
-const database = getDatabase(app);
-
-// Export database
-export { database };
+// Export everything needed by other files
+export { 
+    db, 
+    auth, 
+    collection, 
+    addDoc, 
+    getDoc, 
+    getDocs, 
+    doc, 
+    setDoc, 
+    updateDoc, 
+    onSnapshot, 
+    query,
+    signInWithEmailAndPassword, 
+    createUserWithEmailAndPassword,
+    onAuthStateChanged,
+    signOut
+};
